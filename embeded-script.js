1d87e934631d2f9b7d3dd5085e30c1ff4674cc69
Fumiya Akagi <fumiya.akagi@iridge.jp>
2018-09-10 10:50:56 +0900

    refactor : コメント追加。変数をlet, const化

--- embeded-script.js
+++ embeded-script.js
@@ -1,47 +1,30 @@
-window.MyEmbededProgram = {
-  helloWorld: function() {
-    alert("Hello World!")
-  },
 
-  getTitle: function(element) {
-      return element.getElementsByClassName("media-body")[0]
-                    .getElementsByClassName("media-heading")[0]
-                    .getElementsByTagName("a")[0]
-                    .innerText
-  },
-};
+// アプリタイトルの配列
+const names = []
+
+// key=domのid, value=アプリタイトル文字列
+const namesMap = {}
+
+// key=domのid, value=dom
+const domMap = {}
 
 
-var names = []
-var namesMap = {}
-
-window.onload = function() {
-  var div = document.getElementsByClassName("col-sm-7 applications")[0]
-  
-  var insertHtml = "\
-    <form　onsubmit='doSomething();return false;'>\
-      <input onChange='searchByTitle()' id='searchTitle' type='text' name='app_name'>\
-    </form>"
-    // <button type='button' name='aaa' id='searchButton' onClick='searchByTitle()' value='検索'>検索</button>\
-  div.insertAdjacentHTML('afterbegin', insertHtml)
-  
-  var items = document.getElementsByClassName("media")
-  for (var i = 0; i < items.length; i++) {
-    var id = "media-" + i
-    items[i].setAttribute("id",id);
-    var name = window.MyEmbededProgram.getTitle(items[i])
-    names.push(name)
-    namesMap[id] = name
-  }
+// アプリのタイトルをDOMから取得
+const getTitle = (element) => {
+    return element.getElementsByClassName("media-body")[0]
+                  .getElementsByClassName("media-heading")[0]
+                  .getElementsByTagName("a")[0]
+                  .innerText
 }
 
+// テキストボックスに入力されたタイトルを元に部分一致検索を行う
 function searchByTitle() {
   var div = document.getElementsByClassName("col-sm-7 applications")[0]
   var keys = Object.keys(namesMap)
   var searchTitle = document.getElementById("searchTitle").value
   keys.forEach(function(key){
       var name = namesMap[key]
-      var item = document.getElementById(key)
+      var item = domMap[key]
       if (name.includes(searchTitle)) {
         item.setAttribute("style", "display : block")
       } else {
@@ -49,3 +32,29 @@ function searchByTitle() {
       }      
   })
 }
+
+window.onload = function() {
+  const div = document.getElementsByClassName("col-sm-7 applications")[0]
+  
+  // 検索ワード入力用のテキストボックスを埋め込む
+  const insertHtml = "\
+    <form　onsubmit='doSomething();return false;'>\
+      <input onChange='searchByTitle()' id='searchTitle' type='text' name='app_name'>\
+    </form>"
+  div.insertAdjacentHTML('afterbegin', insertHtml)
+  
+  // アプリ一覧用の配列を作成
+  const items = document.getElementsByClassName("media")
+  for (let i = 0; i < items.length; i++) {
+    // 各アプリのDOMにID設定
+    const id = "media-" + i
+    items[i].setAttribute("id",id);
+    
+    // 検索に用いる配列・連想配列の初期化
+    const name = getTitle(items[i])
+    names.push(name)
+    namesMap[id] = name
+    domMap[id] = this.document.getElementById(id)
+  }
+}
+
